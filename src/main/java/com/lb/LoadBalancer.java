d2ff89b59a6dde8c5d85077a4fc213796334d108
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-03-06 12:41:31 -0800

    Distribute load based on Load Balancer

---
+++ src/main/java/com/lb/LoadBalancer.java
@@ -0,0 +1,96 @@
+package com.lb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by jayavardhanpatil on 03/06/20
+ */
+
+public class LoadBalancer {
+
+    private ArrayList<String> totalServersAsPerWeightage;
+    private Integer position;
+
+    LoadBalancer(){
+        position = 0;
+        totalServersAsPerWeightage = new ArrayList<>();
+    }
+
+    /* Method : getAssignedServerName_RandomWeight
+    *  Paramater : HashMap -> Servers and their weights;
+    *  Description : Randomly distributes the jobs to the servers depending on weights,
+    *                Distribution is approximately correct;
+    *  return : String ServerName.
+    * */
+
+    public String getAssignedServerName_RandomWeight(HashMap<String, Integer> serversWeightMap){
+
+        if(serversWeightMap == null || serversWeightMap.size() == 0){
+            return "No Server Available to distribute";
+        }
+
+        totalServersAsPerWeightage = serveraList(serversWeightMap);
+        return assigntoRandomWeightedServer(totalServersAsPerWeightage);
+    }
+
+    /* Method : getAssignedServerName_RandomWeight
+     *  Paramater : HashMap -> Servers and their weights;
+     *  Description : Distributes the jobs to the servers in round robbin way depending on weights,
+     *                correctly distributes to all servers as per weights
+     *  return : String ServerName.
+     * */
+    public String getAssignedServerName_RoundRobinWeight(HashMap<String, Integer> serversWeightMap){
+        if(serversWeightMap == null || serversWeightMap.size() == 0){
+            return "No Server Available to distribute";
+        }
+        totalServersAsPerWeightage = serveraList(serversWeightMap);
+        return assignWeightedServerRoundRobin(totalServersAsPerWeightage);
+    }
+
+
+    private ArrayList<String> serveraList(HashMap<String, Integer> serversWeightMap){
+        ArrayList<String> servers = new ArrayList<>();
+        for(Map.Entry<String, Integer> data: serversWeightMap.entrySet()){
+            for(int i =0;i<data.getValue();i++) {
+                servers.add(data.getKey());
+            }
+        }
+        return servers;
+    }
+
+
+
+    /* Method : assigntoRandomWeightedServer
+     *  Paramater : ArrayList -> List of Servers;
+     *  Description : Randomly distributes the jobs to the servers depending on weights,
+     *                Distribution is approximately correct;
+     *  return : String ServerName.
+     * */
+    public String assigntoRandomWeightedServer(ArrayList<String> totalServersAsPerWeightage){
+        Random random = new Random();
+        return totalServersAsPerWeightage.get(random.nextInt(totalServersAsPerWeightage.size()));
+    }
+
+
+    /* Method : assignWeightedServerRoundRobin
+     *  Paramater : ArrayList -> List of Servers;
+     *  Description : Distributes the jobs to the servers in round robbin way depending on weights,
+     *                correctly distributes to all servers as per weights
+     *  return : String ServerName.
+     * */
+    public String assignWeightedServerRoundRobin(ArrayList<String> totalServersAsPerWeightage){
+        synchronized (position) {
+            if (position > totalServersAsPerWeightage.size()-1) {
+                position = 0;
+            }
+            String target = totalServersAsPerWeightage.get(position);
+            position++;
+            return target;
+        }
+    }
+
+}
+
---
+++ src/test/java/com/lb/LoadBalancertest.java
@@ -0,0 +1,63 @@
+package com.lb;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * Created by jayavardhanpatil on 03/06/20
+ */
+
+public class LoadBalancertest {
+
+    private HashMap<String, Integer> map;
+    private LoadBalancer loadBalancer;
+    @Before
+    public void setUp(){
+        map = new HashMap<>();
+        map.put("A", 3);
+        map.put("B", 3);
+        map.put("C", 4);
+        map.put("D", 4);
+        map.put("E", 1);
+        loadBalancer = new LoadBalancer();
+    }
+
+    @Test
+    public void getRandomAssignedServer(){
+        System.out.println("Server : " + loadBalancer.getAssignedServerName_RandomWeight(map));
+    }
+
+    @Test
+    public void getRoundRobinAssignedServer(){
+        System.out.println("Server : " + loadBalancer.getAssignedServerName_RoundRobinWeight(map));
+    }
+
+    //test Random assigned server for the load of 1000 jobs
+    @Test
+    public void getRandomAssignedServersForAgivenLoad(){
+        for(int i=0;i<1000;i++) {
+            System.out.println("Server : " + loadBalancer.getAssignedServerName_RandomWeight(map));
+        }
+    }
+
+
+    //test Round robin assigned servers on the load of 1000 jobs
+    @Test
+    public void getRoundrobinAssignedServersForAgivenLoad(){
+        for(int i=0;i<1000;i++) {
+            System.out.println("Server : " + loadBalancer.getAssignedServerName_RoundRobinWeight(map));
+        }
+    }
+
+    @Test()
+    public void getRandomServers(){
+        HashMap<String, Integer> serversWeights = new HashMap<>();
+        serversWeights.put("X", 1);
+        serversWeights.put("Y", 3);
+        for(int i=0;i<1000;i++) {
+            System.out.println("Server : " + loadBalancer.getAssignedServerName_RandomWeight(serversWeights));
+        }
+    }
+}
